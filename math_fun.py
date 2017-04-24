628b19c9e2da122a52e93543c57084cbe700ebc7
AndrewMlamba <andrew.mlamba@outlook.com>
2017-04-24 15:05:58 +0300

    modified code to raise ValueError

--- math_fun.py
+++ math_fun.py
@@ -1,2 +1,8 @@
-def solve_me_first(a, b):
-    return a + b
+class Calculator:
+    def solve_me_first(self, a, b):
+        number_types = (int, float, complex)
+
+        if isinstance(a, number_types) and isinstance(b, number_types):
+            return a + b
+        else:
+            raise ValueError
